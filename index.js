1291b7117797fd982a0e9d6a69c498a28f15afc3
Marilina Roman <roman.marilinaf@gmail.com>
2022-02-24 23:07:54 -0300

    version 0.9: se agregaron modificaciones de sass y seo

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Navbar
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".navbar");
 
@@ -8,4 +9,56 @@ navToggle.addEventListener("click", () => {
     } else {
     navToggle.setAttribute("aria-label", "Abrir menú");
 }
+});
+// Quienes somos seccion categorias
+const categorias = document.querySelectorAll("#categorias .categoria");
+const contenedorPreguntas = document.querySelectorAll('.contenedor_preguntas');
+let categoriaActiva = null;
+
+categorias.forEach((categoria) => {
+	categoria.addEventListener('click', (e) => {
+		categorias.forEach((elemento) => {
+			elemento.classList.remove('activa');
+		});
+
+		e.currentTarget.classList.toggle('activa');
+		categoriaActiva = categoria.dataset.categoria;
+
+
+		// Activo el contenedor de preguntas que corresponde
+		contenedorPreguntas.forEach((contenedor) => {
+			if(contenedor.dataset.categoria === categoriaActiva){
+				contenedor.classList.add('activo');
+			} else {
+				contenedor.classList.remove('activo');
+			}
+		});
+	});
+});
+// Quienes somos seccion preguntas
+const preguntas = document.querySelectorAll('.preguntas .contenedor_pregunta');
+preguntas.forEach((pregunta) => {
+	pregunta.addEventListener('click', (e) => {
+		e.currentTarget.classList.toggle('activa');
+
+		const respuesta = pregunta.querySelector('.respuesta');
+		const alturaRealRespuesta = respuesta.scrollHeight;
+		
+		if(!respuesta.style.maxHeight){
+			// Si esta vacio el maxHeight entonces pone un valor.
+			respuesta.style.maxHeight = alturaRealRespuesta + 'px';
+		} else {
+			respuesta.style.maxHeight = null;
+		}
+
+		// Reinicio las demas preguntas
+		preguntas.forEach((elemento) => {
+			if(pregunta !== elemento){
+				elemento.classList.remove('activa');
+				elemento.querySelector('.respuesta').style.maxHeight = null;
+			}
+		});
+
+
+	});
 });
