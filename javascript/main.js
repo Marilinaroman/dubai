e47515ca68bec149ece857ae9c9e8fd554cebda7
Marilina Roman <roman.marilinaf@gmail.com>
2022-03-20 14:00:26 -0300

    version 1.2.2: se realizaron modificaciones con javascript

--- javascript/main.js
+++ javascript/main.js
@@ -58,7 +58,47 @@ preguntas.forEach((pregunta) => {
 				elemento.querySelector('.respuesta').style.maxHeight = null;
 			}
 		});
-
-
 	});
 });
+
+// Enviar por Whatsapp en contacto
+var enviarMensaje = document.getElementById('WSP');
+console.log(enviarMensaje);
+function mensaje_wsp(){
+	var nombre = document.getElementById("nombre")
+	//<input id="nombreform" type="text" name="name" class="form-control" id="name" placeholder="Nombre" value="Nombre" required>
+	var mail = document.getElementById("email")
+	var fechaTour = document.getElementById("fecha_tour")
+	var tour = document.getElementById("tour")
+	var motivo = document.getElementById("asunto")
+	var mensaje = document.getElementById("consulta")
+
+	console.log(nombre);
+
+	//console.log(nombre.value)
+
+	var mensaje_final
+	mensaje_final ="Hola!,%20Mi%20Nombre%20es:%20" + nombre.value +",%20" 
+	mensaje_final =  mensaje_final + "%20Mi%20Mail%20es:%20" + mail.value
+	mensaje_final =  mensaje_final + "%20Me%20interesa%20el%20tour:%20" + tour.value
+	mensaje_final =  mensaje_final + "%20el%20dia:%20" + fechaTour.value
+	mensaje_final = mensaje_final + ",%20El%20Motivo%20de%20mi%20contacto%20es%20" + motivo.value + ",%20"
+	mensaje_final = mensaje_final + "%20mensaje%20:%20" + mensaje.value
+
+	var mensaje
+	mensaje = "https://api.whatsapp.com/send?phone=+5491136457467&text=" + mensaje_final 
+	//Esto%20es%20una%20prueba
+	enviarMensaje.href = mensaje  
+}
+
+enviarMensaje.addEventListener('click', mensaje_wsp)
+
+// Suscripcion
+function suscripcion (){
+	var mailSuscripcion = document.getElementById("mail_suscripcion").value;
+	if (mailSuscripcion == "") {
+		alert("Debes ingresar tu email"); 
+	} else {
+		alert ("Te suscribiste a nuestra Newsletter!")
+	}
+}
